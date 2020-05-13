758c7c3cb3fd7110c47279e7a5456e2430bc2903
Vasiliy Pupkin <vpupkin@company.com>
2020-05-13 18:26:07 +0300

    вертска

--- js/main.js
+++ js/main.js
@@ -50,3 +50,60 @@ $('.navigation').singlePageNav({
   filter: ':not(.external)',
   easing: 'swing',
 });
+
+/*******    team slick    **************/
+$('.team-slider').slick({
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  dots: true,
+  arrows: false,
+  responsive: [{
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: 750,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 550,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+/*$(window).load(function() {
+    var $container = $('#lightbox');
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
+    });
+    $('.cat a').click(function() {
+        $('.cat .active').removeClass('active');
+        $(this).addClass('active');
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        return false;
+    });
+
+});*/
